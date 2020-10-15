289c92d1bfbfdbe00797a03f8d2dc2ea5ca0778a
bonnette <bonnette_l@yahoo.com>
2020-10-15 13:57:04 -0500

    unsettled

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -15,7 +15,7 @@
  * in order to get wind speed , wind gust and more.
  * From this response I extract weather data and display it in a window on a raspberry pi
  * Written by Larry Bonnette October 2020
- */
+ */ 
 
 #include "math.h"
 #include "mainwindow.h"
